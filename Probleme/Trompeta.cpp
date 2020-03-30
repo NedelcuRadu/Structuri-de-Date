b0f2dbaa62a57821196de138c4520282e752b9a1
Nedelcu Radu <43296487+NedelcuRadu@users.noreply.github.com>
2020-03-30 13:57:24 +0300

    Create Trompeta.cpp

---
+++ Probleme/Trompeta.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+ifstream in("trompeta.in");
+ofstream out("trompeta.out");
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    string A,B;
+    int N, M;
+    int eliminate = 0;
+    in >> N >> M;
+    int deeliminat = N - M;
+    in>>A;
+    for (auto &i : A){
+            while(!B.empty() && eliminate < deeliminat && i> B.back()) {
+                B.pop_back();
+                eliminate++;
+            }
+        B.push_back(i);
+    }
+    out<<B;
+
+    return 0;
+}
